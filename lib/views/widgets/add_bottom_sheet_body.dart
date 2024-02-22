7b56fd309e81f15f4d7f75816fe17992ffd87fae
Adham Elagaty <adhamalagaty@gmail.com>
2024-02-23 00:55:23 +0200

    Add BlocConsumer to AddNoteBottomSheetBody and create blocObserver

--- lib/views/widgets/add_bottom_sheet_body.dart
+++ lib/views/widgets/add_bottom_sheet_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
 
 class AddNoteBottomSheetBody extends StatelessWidget {
@@ -8,15 +11,25 @@ class AddNoteBottomSheetBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: 6,
-        left: 12,
-        right: 12,
-        bottom: MediaQuery.of(context).viewInsets.bottom + 16,
-      ),
-      child: const SingleChildScrollView(
-        child: AddNoteForm(),
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        } else if (state is AddNoteFailure) {}
+      },
+      builder: (context, state) => ModalProgressHUD(
+        inAsyncCall: false,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 6,
+            left: 12,
+            right: 12,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+          ),
+          child: const SingleChildScrollView(
+            child: AddNoteForm(),
+          ),
+        ),
       ),
     );
   }
--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custome_button.dart';
 import 'package:notes_app/views/widgets/custome_text_field.dart';
 
@@ -40,7 +43,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             child: CustomeTextField(
               hintText: "Write Here....",
               labelText: "Content",
-              height: 200,
+              height: 300,
               onSaved: (value) {
                 subTitle = value;
               },
@@ -51,6 +54,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                NoteModel noteModel = NoteModel(
+                  title: title!,
+                  subTitle: subTitle!,
+                  date: DateTime.now().toString(),
+                  color: Colors.blue.value,
+                );
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
