0eccf16fdb4454c7d3714d7767ea876e4b7597cb
Adham Elagaty <adhamalagaty@gmail.com>
2024-02-25 22:16:47 +0200

    create methods in edit note form to chek on title & SubTitle and method to update note

--- lib/views/widgets/edit_note_form.dart
+++ lib/views/widgets/edit_note_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custome_button.dart';
 import 'package:notes_app/views/widgets/custome_text_field.dart';
@@ -40,18 +42,7 @@ class _EditNoteFormState extends State<EditNoteForm> {
                 maxLength: 50,
                 onChanged: (data) {
                   title = data;
-                  if (title != widget.noteModel.title &&
-                      EditNoteForm.formState.currentState!.validate() &&
-                      !isValidT) {
-                    isValidT = true;
-                    setState(() {});
-                  } else if (title == widget.noteModel.title && isValidT) {
-                    isValidT = false;
-                    setState(() {});
-                  } else if (!EditNoteForm.formState.currentState!.validate()) {
-                    isValidT = false;
-                    setState(() {});
-                  }
+                  checkTitleValidation();
                 },
               ),
             ),
@@ -62,22 +53,10 @@ class _EditNoteFormState extends State<EditNoteForm> {
                 labelText: "Content",
                 initialValue: widget.noteModel.subTitle,
                 // width: 420,
-                height: 100,
+                height: 400,
                 onChanged: (data) {
                   subTitle = data;
-                  if (subTitle != widget.noteModel.subTitle &&
-                      EditNoteForm.formState.currentState!.validate() &&
-                      !isValidSubT) {
-                    isValidSubT = true;
-                    setState(() {});
-                  } else if (subTitle == widget.noteModel.subTitle &&
-                      isValidSubT) {
-                    isValidSubT = false;
-                    setState(() {});
-                  } else if (!EditNoteForm.formState.currentState!.validate()) {
-                    isValidSubT = false;
-                    setState(() {});
-                  }
+                  checkSubTitleValidation();
                 },
               ),
             ),
@@ -86,7 +65,11 @@ class _EditNoteFormState extends State<EditNoteForm> {
               child: CustomeButton(
                 isLoading: false,
                 textButton: "Update",
-                onTap: () {},
+                onTap: () {
+                  updateNoteData();
+                  BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                  Navigator.pop(context);
+                },
                 color: isValidSubT || isValidT
                     ? const Color(kPrimaryColor)
                     : Colors.grey,
@@ -96,5 +79,41 @@ class _EditNoteFormState extends State<EditNoteForm> {
         ),
       ),
     );
+  }
+
+  void checkSubTitleValidation() {
+    if (subTitle != widget.noteModel.subTitle &&
+        EditNoteForm.formState.currentState!.validate() &&
+        !isValidSubT) {
+      isValidSubT = true;
+      setState(() {});
+    } else if (subTitle == widget.noteModel.subTitle && isValidSubT) {
+      isValidSubT = false;
+      setState(() {});
+    } else if (!EditNoteForm.formState.currentState!.validate()) {
+      isValidSubT = false;
+      setState(() {});
+    }
+  }
+
+  void checkTitleValidation() {
+    if (title != widget.noteModel.title &&
+        EditNoteForm.formState.currentState!.validate() &&
+        !isValidT) {
+      isValidT = true;
+      setState(() {});
+    } else if (title == widget.noteModel.title && isValidT) {
+      isValidT = false;
+      setState(() {});
+    } else if (!EditNoteForm.formState.currentState!.validate()) {
+      isValidT = false;
+      setState(() {});
+    }
+  }
+
+  void updateNoteData() {
+    widget.noteModel.title = title ?? widget.noteModel.title;
+    widget.noteModel.subTitle = subTitle ?? widget.noteModel.subTitle;
+    widget.noteModel.save();
   }
 }
